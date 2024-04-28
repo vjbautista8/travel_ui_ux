766e266502441e959118a171b0e44738867bc0b9
vjbautista8 <vjbautista8@gmail.com>
2024-04-29 06:25:49 +0800

    fixed images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = (process.env.NODE_ENV = 'production');
 const nextConfig = {
-  basePath: '/travel_ui_ux',
+  basePath: isProd ? '/travel_ui_ux' : '',
   reactStrictMode: true,
 };
 
