79123de370f5341d535e06a141f6bae509fb16cd
vjbautista8 <vjbautista8@gmail.com>
2024-04-29 06:41:37 +0800

    hero images

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,37 +1,37 @@
-import { FEATURES } from '@/constants'
-import Image from 'next/image'
-import React from 'react'
+import { FEATURES } from '@/constants';
+import Image from 'next/image';
+import React from 'react';
 
 const Features = () => {
   return (
-    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container padding-container relative w-full flex justify-end">
-        <div className="flex flex-1 lg:min-h-[900px]">
+    <section className='flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
+      <div className='max-container padding-container relative w-full flex justify-end'>
+        <div className='flex flex-1 lg:min-h-[900px]'>
           <Image
-            src="/phone.png"
-            alt="phone"
+            src='/travel_ui_ux/phone.png'
+            alt='phone'
             width={440}
             height={1000}
-            className="feature-phone"
+            className='feature-phone'
           />
         </div>
 
-        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+        <div className='z-20 flex w-full flex-col lg:w-[60%]'>
           <div className='relative'>
             <Image
-              src="/camp.svg"
-              alt="camp"
+              src='/travel_ui_ux/camp.svg'
+              alt='camp'
               width={50}
               height={50}
-              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
+              className='absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]'
             />
-            <h2 className="bold-40 lg:bold-64">Our Features</h2>
+            <h2 className='bold-40 lg:bold-64'>Our Features</h2>
           </div>
-          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
+          <ul className='mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20'>
             {FEATURES.map((feature) => (
-              <FeatureItem 
+              <FeatureItem
                 key={feature.title}
-                title={feature.title} 
+                title={feature.title}
                 icon={feature.icon}
                 description={feature.description}
               />
@@ -40,29 +40,27 @@ const Features = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 type FeatureItem = {
   title: string;
   icon: string;
   description: string;
-}
+};
 
 const FeatureItem = ({ title, icon, description }: FeatureItem) => {
   return (
-    <li className="flex w-full flex-1 flex-col items-start">
-      <div className="rounded-full p-4 lg:p-7 bg-green-50">
-        <Image src={icon} alt="map" width={28} height={28} />
+    <li className='flex w-full flex-1 flex-col items-start'>
+      <div className='rounded-full p-4 lg:p-7 bg-green-50'>
+        <Image src={icon} alt='map' width={28} height={28} />
       </div>
-      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
-        {title}
-      </h2>
-      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+      <h2 className='bold-20 lg:bold-32 mt-5 capitalize'>{title}</h2>
+      <p className='regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none'>
         {description}
       </p>
     </li>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
