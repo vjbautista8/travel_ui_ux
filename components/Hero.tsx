361f317cb44dbf83ea5053ba46e5d7c596f71819
vjbautista8 <vjbautista8@gmail.com>
2024-04-29 06:32:08 +0800

    fixed images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
       <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
         <Image
-          src={`${config.basePth}/camp.svg`}
+          src={`${config.basePath}/camp.svg`}
           alt='camp'
           width={50}
           height={50}
