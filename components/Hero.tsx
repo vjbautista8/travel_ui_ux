27d5263d35fb422062853feb66719ea5e26135a0
vjbautista8 <vjbautista8@gmail.com>
2024-04-29 06:34:53 +0800

    fixed image-heros

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import Button from './Button';
-const config = require('../next.config');
+
 const Hero = () => {
+  const config = require('../next.config');
   return (
     <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
       <div className='hero-map' />
