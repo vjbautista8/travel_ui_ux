c319bfd721e3a8a8505dead8110ae6562b85d877
vjbautista8 <vjbautista8@gmail.com>
2024-04-29 06:39:18 +0800

    hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,14 +2,13 @@ import Image from 'next/image';
 import Button from './Button';
 
 const Hero = () => {
-  const config = require('../next.config');
   return (
     <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
       <div className='hero-map' />
 
       <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
         <Image
-          src={`${config.basePath}/camp.svg`}
+          src='/travel_ui_ux/camp.svg'
           alt='camp'
           width={50}
           height={50}
